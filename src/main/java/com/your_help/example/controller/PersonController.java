34912d6b7394dffe6f9b78b6cff1788af241af95
davidburla <burladavid@gmail.com>
2021-12-15 22:33:27 +0200

    Change cross control

--- src/main/java/com/your_help/example/controller/PersonController.java
+++ src/main/java/com/your_help/example/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://proiect-api-demo.com:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class PersonController {
